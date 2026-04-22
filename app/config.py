034b1dc4ca73cfd02e231e14d9e6560dbee51c09
hakan192 <101261081+hakan192@users.noreply.github.com>
2026-04-23 00:18:41 +0300

    Change default managed device port to 443

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     celery_broker_url: str = "redis://redis:6379/1"
     celery_result_backend: str = "redis://redis:6379/2"
 
-    fortiweb_base_url: str = "https://3.236.139.71:8443"
+    fortiweb_base_url: str = "https://3.236.139.71:443"
     fortiweb_token: str = "change-me"
     fortiweb_verify_ssl: bool = False
     fortiweb_config_endpoint: str = "/api/v2.0/cmdb/waf"
